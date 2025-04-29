70b55bcc6a23403b6f8d98b33c5834435e36d95d
Zili Wang <zili.wang@tum.de>
2025-04-29 23:45:00 +0000

    update

--- rlbs/reles_env.py
+++ rlbs/reles_env.py
@@ -1,4 +1,5 @@
 import time
+import os
 import numpy as np
 import mpsched
 
@@ -14,6 +15,7 @@ class Env:
         self.last_state = None
 
     def get_state(self):
+        print("[DEBUG] Getting current subflow state")
         subflows = mpsched.get_sub_info(self.fd)
         state = []
         for s in subflows:
@@ -23,10 +25,21 @@ class Env:
             state.append([segs_out, 1e6 / rtt, cwnd, unacked, retrans])
         state = np.array(state, dtype=np.float32)
         self.last_state = state
+        print("[DEBUG] Current state:", state)
         return state
 
     def step(self, action):
+        print("[DEBUG] Step with action:", action)
+        # 设置调度器动作
         mpsched.set_seg([self.fd] + list(map(int, action)))
+
+        # ✅ 实际发送数据（触发 TCP/MPTCP 路径）
+        try:
+            os.write(self.fd, b'x' * 1400)  # 立即发送数据
+            print("[DEBUG] Sent 1400 bytes to server")
+        except Exception as e:
+            print("[ERROR] os.write failed:", e)
+
         time.sleep(self.time)
 
         subflows = mpsched.get_sub_info(self.fd)
@@ -37,6 +50,7 @@ class Env:
                 rtt = 1
             next_state.append([segs_out, 1e6 / rtt, cwnd, unacked, retrans])
         next_state = np.array(next_state, dtype=np.float32)
+        print("[DEBUG] Next state:", next_state)
 
         if self.last_state is None:
             reward = 0
@@ -45,5 +59,6 @@ class Env:
             rtt_term = next_state[:, 1].sum()
             reward = seg_diff + self.alpha * rtt_term
 
+        print("[DEBUG] Reward:", reward)
         self.last_state = next_state
         return next_state, reward
