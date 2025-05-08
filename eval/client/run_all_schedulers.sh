81bc6b69f5a3ccdd32bfdd98d6371b81ddffe6bb
Zili Wang <zili.wang@tum.de>
2025-05-08 13:46:14 +0000

    update

--- eval/client/run_all_schedulers.sh
+++ eval/client/run_all_schedulers.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-SCHEDULERS=("default" "roundrobin" "redundant" "blest" "ecf" "reles" "falcon")
+#SCHEDULERS=("default" "roundrobin" "redundant" "blest" "ecf" "reles" "falcon")
+SCHEDULERS=("default" "roundrobin" "redundant" "blest" "ecf")
 NS_NAME="ns-mptcp"
 SCRIPT_PATH="/home/vagrant/sender_logger_file.py"
-
-# 默认轮数为 3，允许通过第一个参数传入新值
 ROUNDS=${1:-3}
 
 for sched in "${SCHEDULERS[@]}"; do
     echo "==== Testing scheduler: $sched (Rounds: $ROUNDS) ===="
     sudo sysctl -w net.mptcp.mptcp_scheduler=$sched
 
-#    LOGFILE="result_${sched}_$(date +%Y%m%d_%H%M%S).log"
     sudo ip netns exec $NS_NAME python3 $SCRIPT_PATH $ROUNDS
+    status=$?
 
-    echo "==== Scheduler $sched Test Done ===="
+    if [ $status -ne 0 ]; then
+        echo "[Error] Scheduler $sched failed to complete sending (exit code $status)"
+    else
+        echo "==== Scheduler $sched Test Done ===="
+    fi
     echo
     sleep 2
 done
